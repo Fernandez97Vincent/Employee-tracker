ee118197bae19bcf9e3341ece9be6140708a65dd
Vincent Fernandez <fernandez97vincent@gmail.com>
2022-10-04 17:33:53 -0700

    Addded the first choice to server.js

--- server.js
+++ server.js
@@ -17,3 +17,55 @@ const connection = mysql.createConnection({
     password: 'hotpocket',
     database: 'employees_db'
 });
+// checking to see if server.js works
+//console.log(firstChoice)
+// ask user what they would like to do
+function firstChoice() {
+    inquirer.prompt({
+        type: 'list',
+        name: 'task',
+        message: 'Please choose an option',
+        choices: [
+            "View Employees",
+            "View Employees by Department",
+            "Add Employee",
+            "Remove Employee",
+            "Update Employee Role",
+            "Add Role",
+            "Stop"
+        ]
+    })
+    // create a switch statement that sets the options to functions
+    .then(function ({ action }) {
+        switch (action) {
+            case 'View Employees':
+                viewEmployees();
+                break;
+            
+            case 'View Employee by Department':
+                viewDepartment();
+                break;
+
+            case 'Add Employee':
+                addEmployee();
+                break;
+
+            case 'Remove Employee':
+                removeEmployee();
+                break;
+            
+            case 'Update Employee Role':
+                updateEmployee();
+                break;
+
+            case 'Add Role':
+                addRole();
+                break;
+            
+            case 'Stop':
+                connection.end();
+                break;
+
+        }
+    })
+}
